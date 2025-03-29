33a4b7b8d28eb856e850ae37cbcb4567943d312a
Northland <162111816+NorthlandWow@users.noreply.github.com>
2025-03-29 18:23:15 +0100

    cambio nombre proyecto

--- Codigo/plantillas/header.php
+++ Codigo/plantillas/header.php
@@ -1,12 +1,11 @@
 <header>
-    <a href="/Proyecto-FCT/index.php">
-        <img src="/Proyecto-FCT/imagenes/logo_provisional.png" alt="icono" width="150px">
+    <a href="/Proyecto_FCT/Codigo/index.php">
+        <img src="/Proyecto_FCT/imagenes/logo_provisional.png" alt="icono" width="150px">
     </a>
     <nav>
         <ul>
-            <li><a href="/Proyecto-FCT/index.php">Inicio</a></li>
-            <li><a href="/Proyecto-FCT/Codigo/validaciones/registro.php">Registro</a></li>
-            <li><a href="/Proyecto-FCT/contacto.php">Contacto</a></li>
+            <li><a href="/Proyecto_FCT/Codigo/index.php">Inicio</a></li>
+            <li><a href="/Proyecto_FCT/Codigo/validaciones/registro.php">Registro</a></li>
         </ul>
     </nav>
 </header>
