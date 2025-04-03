bc496edd2d64d658fee87054dfcf9c742a0bb5fe
Northland <162111816+NorthlandWow@users.noreply.github.com>
2025-04-03 17:09:10 +0200

    pruebas con admin

--- Codigo/admin.php
+++ Codigo/admin.php
@@ -10,35 +10,37 @@
 </head>
 
 <body>
-<?php require_once '../Codigo/plantillas/header.php'; ?>
+    <?php require_once '../Codigo/plantillas/header.php'; ?>
     <main class="contenedor">
-        <!-- Seccion izquierda busqueda -->
-        <div class="seccion busqueda">
-            <h2>Busqueda de paciente</h2>
-            <?php require_once '../Codigo/validaciones/busqueda.php'; ?>
-        </div>
+        <div class="orden arriba">
+            <!-- Seccion izquierda busqueda -->
+            <div class="seccion busqueda">
+                <h2>Busqueda de paciente</h2>
+                <?php require_once '../Codigo/validaciones/busqueda.php'; ?>
+            </div>
 
-        <!-- Seccion derecha tratamiento -->
-        <div class="seccion tratamiento">
-            <h2>Listado de tratamientos</h2>
-            <?php require_once '../Codigo/validaciones/tratamientos.php'; ?>
+            <!-- Seccion derecha tratamiento -->
+            <div class="seccion tratamiento">
+                <h2>Listado de tratamientos</h2>
+                <?php require_once '../Codigo/validaciones/tratamientos.php'; ?>
+            </div>
         </div>
-
-        <!-- Seccion para administrar promociones -->
-         <div class="seccion promociones">
-            <?php require_once '../Codigo/validaciones/promociones/promociones_form.php'; ?>
-         </div>
-        <div class="seccion promociones-lista">
-            <h2>Promociones Existentes</h2>
-            <?php require_once '../Codigo/validaciones/promociones/promociones_lista.php'; ?>
+        <div class="orden abajo">
+            <!-- Seccion para administrar promociones -->
+            <div class="seccion promociones">
+                <?php require_once '../Codigo/validaciones/promociones/promociones_form.php'; ?>
+            </div>
+            <div class="seccion promociones-lista">
+                <h2>Promociones Existentes</h2>
+                <?php require_once '../Codigo/validaciones/promociones/promociones_lista.php'; ?>
+            </div>
+        </div>
+        <!-- Seccion para ver las proximas citas -->
+        <div class="historial">
+            <h2>Historial</h2>
+            <?php require_once '../Codigo/validaciones/historial.php'; ?>
         </div>
     </main>
-
-    <!-- Seccion para ver las proximas citas -->
-    <div class="historial">
-        <h2>Historial</h2>
-        <?php require_once '../Codigo/validaciones/historial.php'; ?>
-    </div>
     <?php require_once '../Codigo/plantillas/footer.php'; ?>
 </body>
 
--- Codigo/validaciones/promociones/promociones_form.php
+++ Codigo/validaciones/promociones/promociones_form.php
@@ -1,13 +1,18 @@
 <h2>Agregar Promoción</h2>
 <form action="validaciones/promociones_admin.php" method="POST" enctype="multipart/form-data">
-    <label for="titulo">Título:</label>
-    <input type="text" id="titulo" name="titulo" required>
-
-    <label for="descripcion">Descripción:</label>
-    <textarea id="descripcion" name="descripcion" required></textarea>
-
-    <label for="imagen">Imagen:</label>
-    <input type="file" id="imagen" name="imagen" accept="image/*" required>
-
+    <table>
+        <tr>
+            <td><label for="titulo">Título:</label></td>
+            <td><input type="text" id="titulo" name="titulo" required></td>
+        </tr>
+        <tr>
+            <td><label for="descripcion">Descripción:</label></td>
+            <td><textarea id="descripcion" name="descripcion" required></textarea></td>
+        </tr>
+        <tr>
+            <td><label for="imagen">Imagen:</label></td>
+            <td><input type="file" id="imagen" name="imagen" accept="image/*" required></td>
+        </tr>
+    </table>
     <button type="submit">Agregar Promoción</button>
 </form>
