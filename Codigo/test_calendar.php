c7cc12be1ad69a6c1e8776444fb0dbfa354237d4
NorthlandWow <d4n1.g0d0y@gmail.com>
2025-04-22 13:01:35 +0200

    modificados comentarios

--- Codigo/test_calendar.php
+++ Codigo/test_calendar.php
@@ -1,4 +1,5 @@
 <?php
+// Test para crear un evento en Google Calendar
 require_once __DIR__ . '/googleCalendar/google_calendar.php';
 
 $fecha = '2025-04-14';
@@ -6,6 +7,6 @@ $horaInicio = '10:00';
 $horaFin = '10:30';
 $descripcion = 'Prueba de evento automático';
 
-$link = crearEvento($fecha, $horaInicio, $horaFin, $descripcion, $idPaciente);
+$link = crearEvento($fecha, $horaInicio, $horaFin, $descripcion, $idPaciente, $bloqueada);
 echo "Evento creado: <a href='$link' target='_blank'>Ver en Calendar</a>";
 ?>
