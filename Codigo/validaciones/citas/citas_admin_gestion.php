b1c2fe65b9a0e8254e01891f9a69476d4a2ae2a5
NorthlandWow <d4n1.g0d0y@gmail.com>
2025-05-06 12:17:47 +0200

    correcion de error en fecha a la hora de crear pacientes temporales

--- Codigo/validaciones/citas/citas_admin_gestion.php
+++ Codigo/validaciones/citas/citas_admin_gestion.php
@@ -44,7 +44,7 @@ $fechaFormateada = $formatter->format($fecha);
 // Mostrar formulario
 echo '<h3>Horarios disponibles para ' . $fechaFormateada . ':</h3>';
 echo '<form method="POST" action="/Codigo/validaciones/citas/crear_cita_admin.php">';
-echo '<input type="hidden" name="fecha" value="' . $fechaFormateada . '">';
+echo '<input type="hidden" name="fecha" value="' . $fechaSeleccionada . '">';
 
 foreach ($tramos as $hora) {
     $horaCompleta = $hora . ':00';
