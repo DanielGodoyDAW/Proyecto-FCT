26b9b06e7403271affd9f4a521d0678f4739dbea
Northland <162111816+NorthlandWow@users.noreply.github.com>
2025-05-25 14:37:12 +0200

    correccion responsive en agregar servicio

--- Codigo/index.php
+++ Codigo/index.php
@@ -14,6 +14,7 @@
 <body>
     <?php require_once './plantillas/header.php'; ?>
     <main>
+        <!-- Formulario de inicio de sesion -->
         <div class="contenedorLogin" id="login">
             <form class="formularioLogin" action="./validaciones/validacionUsuario.php" id="validacionUsuario" method="post">
                 <table class="tablaLogin">
@@ -27,6 +28,7 @@
                         <td><input type="password" id="c2" name="password" placeholder="Contraseña" minlength="8" maxlength="20" required></td>
                     </tr>
                 </table>
+                <!-- Error si escribimos algun campo erroneo -->
                 <?php if (isset($_GET['error'])) : ?>
                     <div id="error-message">Usuario o contraseña incorrectos</div>
                 <?php endif; ?>
@@ -35,6 +37,7 @@
             </form>
             <p><a class="decoracion" href="#" onclick="mostrarRecuperar()">¿Olvidaste tu contraseña?</a></p>
         </div>
+        <!-- div oculto para el apartado de recuperar contraseña -->
         <div class="contenedorLogin" id="recuperar">
             <h2>Recuperar Contraseña</h2>
             <form action="/Codigo/validaciones/recuperarContra/procesar_recuperacion.php" method="post">
@@ -50,6 +53,7 @@
             </form>
             <p><a class="decoracion" href="#" onclick="mostrarInicio()">Volver a Inicio</a></p>
         </div>
+        <!-- Script para mostrar/ocultar formularios -->
         <?php if (isset($_GET['recuperar'])) {
             echo "<script>mostrarRecuperar();</script>";
         } ?>
